66598be0fe85da300100a3b907172f85ab334e06
Om-Midya <dukemidya7521@gmail.com>
2026-07-07 04:40:45 +0530

    Add React integration and anime.js

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 export default defineConfig({
   output: 'static',
+
   markdown: {
     shikiConfig: {
       themes: { light: 'github-light', dark: 'github-dark' },
       defaultColor: false,
     },
   },
+
+  integrations: [react()],
 });
